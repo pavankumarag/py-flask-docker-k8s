0be45d0f094301c7f8a4e2260ae98a24dfbbfe24
Pavan Kumar A G <pavankumarag@Shwetha-MacBook-Air.local>
2023-06-24 19:48:01 +0530

    first working code

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/new')
+def hello():
+    return "<h1>From flask</h1>"
+
+@app.route('/new1')
+def hello1():
+    return "<h1>From flask new1</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
